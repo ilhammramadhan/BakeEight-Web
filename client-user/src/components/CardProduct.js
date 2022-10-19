4ef9788beb89a662c1b1a833a29c5e045262bb4f
ilhammramadhan <ilhamram20@gmail.com>
2022-10-19 17:59:59 +0700

    set Loading while fetch

--- client-user/src/components/CardProduct.js
+++ client-user/src/components/CardProduct.js
@@ -4,24 +4,35 @@ import {useEffect} from 'react'
 import CardCol from "./CardCol"
 import {useSelector,useDispatch} from 'react-redux'
 import { fetchProducts } from "../store/actions/productAction"
+import ClimbingBoxLoader from"react-spinners/ClimbingBoxLoader"
 
 
 const CardProduct = () => {
-  const {data : item} = useSelector((state) => state)
+  const {data : item , loading} = useSelector((state) => state)
   const dispatch = useDispatch()
 
   useEffect(()=> {
     dispatch(fetchProducts('http://localhost:5000/Item'))
-  },[])
+  },[dispatch])
   return (
     <Container className="mt-4">
-      <Row>
+      {
+        loading ? (
+          <div className="d-flex justify-content-center">
+            <ClimbingBoxLoader color="#36d7b7" size={20} />
+          </div>
+          
+        ) : (
+          <Row>
         {
           item.map((cake) => {
             return <CardCol cake={cake} />
           })
         }
       </Row>
+        )
+      }
+      
     </Container>
 
   )
--- client-user/src/store/actions/actionType.js
+++ client-user/src/store/actions/actionType.js
@@ -1,5 +1,6 @@
 export const FETCH_PRODUCT_SUCCESS = 'product/fetchSuccess'
-
+export const FETCH_PRODUCTDETAIL_SUCCESS = 'product/fetchDetailSuccess'
+export const LOADING_WATCHER = 'loading/setLoading'
 
 
 
